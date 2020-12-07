2b20b7b55d153ab8a696d67ebbee11646c141841
SYONG <ghdgus24@naver.com>
2020-12-07 10:43:52 +0900

    res.send / babel / nodemon 추가

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express');
+import express from "express";
+
 const app = express();
 
 const PORT = 4000;
 
-const handleListening = () => {
-  console.log(`Listening on: http://localhost:${PORT}`);
-}
+const handleListening = () =>
+	console.log(`Listening on: http://localhost:${PORT}`);
+const handleHome = (req, res) => res.send("hello from home");
+const handleProfile = (req, res) => res.send("you are in my profile");
 
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
 app.listen(PORT, handleListening);

4d75310f14a34a535bbf595485b8e65b5743f61b
홍승용 <ghdgus24@naver.com>
2020-12-08 18:14:11 +0900

    controller 분리

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,12 +1,19 @@
 import express from "express";
+import { join, login, logout } from "../controllers/userController";
+import { home, search } from "../controllers/videoController";
 import routes from "../routes";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.home, (req, res) => res.send("home"));
-globalRouter.get(routes.join, (req, res) => res.send("join"));
-globalRouter.get(routes.login, (req, res) => res.send("login"));
-globalRouter.get(routes.logout, (req, res) => res.send("logout"));
-globalRouter.get(routes.search, (req, res) => res.send("search"));
+// global
+// get 함수 앞의 인자 routes.home은 URL 주소, 뒤에 인자 home은 res.send 함수
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.search, search);
+
+
+// user
+globalRouter.get(routes.join, join);
+globalRouter.get(routes.login, login);
+globalRouter.get(routes.logout, logout);
 
 export default globalRouter;

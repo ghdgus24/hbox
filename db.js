98b215fa173d255385129e1d4fa74f7b29bd97c2
SYONG <ghdgus24@naver.com>
2020-12-16 14:53:45 +0900

    홈화면에서 비디오목록 가져오기 완료

---
+++ db.js
@@ -0,0 +1,41 @@
+export const videos = [
+	{
+		id: 324393,
+		title: "Video awesome",
+		description: "This is something I love",
+		views: 23,
+		videoFile:
+			"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+		creator: {
+			id: 121212,
+			name: "SYONG",
+			email: "syong@hong.com",
+		},
+	},
+	{
+		id: 456123,
+		title: "Video soso",
+		description: "This is something I love",
+		views: 23,
+		videoFile:
+			"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+		creator: {
+			id: 121212,
+			name: "SYONG",
+			email: "syong@hong.com",
+		},
+	},
+	{
+		id: 789456,
+		title: "Video anything",
+		description: "This is something I love",
+		views: 23,
+		videoFile:
+			"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+		creator: {
+			id: 121212,
+			name: "SYONG",
+			email: "syong@hong.com",
+		},
+	},
+];

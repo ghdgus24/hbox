63cb6be1e23418810c871ebc1fe0244104dace46
SYONG <ghdgus24@naver.com>
2020-12-16 17:34:27 +0900

    로그인 여부에 따른 헤더다르게 보여주기

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ export const videos = [
 		description: "This is something I love",
 		views: 23,
 		videoFile:
-			"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+			"https://vod-progressive.akamaized.net/exp=1608112727~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F1960%2F18%2F459802291%2F2032938172.mp4~hmac=843258936576c46c75e795437b8f4ce45fa2f7acd6a7aa9ed4c9a86a8372838e/vimeo-prod-skyfire-std-us/01/1960/18/459802291/2032938172.mp4?filename=Forest+-+49981.mp4",
 		creator: {
 			id: 121212,
 			name: "SYONG",
@@ -18,7 +18,7 @@ export const videos = [
 		description: "This is something I love",
 		views: 23,
 		videoFile:
-			"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+			"https://vod-progressive.akamaized.net/exp=1608112784~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4696%2F17%2F448480646%2F1969407398.mp4~hmac=853be1cafef8164a1df75a99fb033b22c3b27dfd1d8d8d67abea8ed447f13104/vimeo-prod-skyfire-std-us/01/4696/17/448480646/1969407398.mp4?filename=Alone+-+46637.mp4",
 		creator: {
 			id: 121212,
 			name: "SYONG",
@@ -31,7 +31,7 @@ export const videos = [
 		description: "This is something I love",
 		views: 23,
 		videoFile:
-			"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+			"https://vod-progressive.akamaized.net/exp=1608112811~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F1302%2F17%2F431511648%2F1876144211.mp4~hmac=5bc5d10c301c02a718a48a0a1036551c173246969f541b791d4469bf92fbf106/vimeo-prod-skyfire-std-us/01/1302/17/431511648/1876144211.mp4?filename=Nature+-+42420.mp4",
 		creator: {
 			id: 121212,
 			name: "SYONG",

5757561b9900d3ee2cddbfb5d19e0adb97c45fb9
hongsyong92 <ghdgus24@naver.com>
2020-12-24 17:59:02 +0900

    video model 생성

---
+++ models/Video.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const VideoSchema = new mongoose.Schema({
+	fileURL: {
+		type: String,
+		required: "File URL is required", // required 가 충족되지 못하면 나오는 에러메세지
+	},
+	title: {
+		type: String,
+		required: "Title is required",
+	},
+	description: String,
+	views: {
+		type: Number,
+		default: 0,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+const model = mongoose.model("Video", VideoSchema);
+export default model;

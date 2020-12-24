2e5c8e0c5d964b4a61a18742f1b0f59224c6c014
hongsyong92 <ghdgus24@naver.com>
2020-12-24 18:03:52 +0900

    깃 레포 주소 변경 확인 커밋

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, {
@@ -13,7 +12,7 @@ mongoose.connect(process.env.MONGO_URL, {
 const db = mongoose.connection;
 const handleOpen = () => console.log("✅ Connected to DB");
 const handleError = (error) =>
-	console.log(`❌  Error on DB Connection : ${error}`);
+	console.log(`❌  Error on DB Connection: ${error}`);
 
 db.once("open", handleOpen);
 db.on("error", handleError);

736fa62fbaa9e0b8cdd1940a7996b42601d6747e
hongsyong92 <ghdgus24@naver.com>
2020-12-24 17:02:07 +0900

    mongoDB 연결, mongoose 설치, .env 추가

--- db.js
+++ db.js
@@ -1,41 +1,19 @@
-export const videos = [
-	{
-		id: 324393,
-		title: "Video awesome",
-		description: "This is something I love",
-		views: 23,
-		videoFile:
-			"https://vod-progressive.akamaized.net/exp=1608112727~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F1960%2F18%2F459802291%2F2032938172.mp4~hmac=843258936576c46c75e795437b8f4ce45fa2f7acd6a7aa9ed4c9a86a8372838e/vimeo-prod-skyfire-std-us/01/1960/18/459802291/2032938172.mp4?filename=Forest+-+49981.mp4",
-		creator: {
-			id: 121212,
-			name: "SYONG",
-			email: "syong@hong.com",
-		},
-	},
-	{
-		id: 456123,
-		title: "Video soso",
-		description: "This is something I love",
-		views: 23,
-		videoFile:
-			"https://vod-progressive.akamaized.net/exp=1608112784~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4696%2F17%2F448480646%2F1969407398.mp4~hmac=853be1cafef8164a1df75a99fb033b22c3b27dfd1d8d8d67abea8ed447f13104/vimeo-prod-skyfire-std-us/01/4696/17/448480646/1969407398.mp4?filename=Alone+-+46637.mp4",
-		creator: {
-			id: 121212,
-			name: "SYONG",
-			email: "syong@hong.com",
-		},
-	},
-	{
-		id: 789456,
-		title: "Video anything",
-		description: "This is something I love",
-		views: 23,
-		videoFile:
-			"https://vod-progressive.akamaized.net/exp=1608112811~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F1302%2F17%2F431511648%2F1876144211.mp4~hmac=5bc5d10c301c02a718a48a0a1036551c173246969f541b791d4469bf92fbf106/vimeo-prod-skyfire-std-us/01/1302/17/431511648/1876144211.mp4?filename=Nature+-+42420.mp4",
-		creator: {
-			id: 121212,
-			name: "SYONG",
-			email: "syong@hong.com",
-		},
-	},
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+	useNewUrlParser: true,
+	useFindAndModify: false,
+	useUnifiedTopology: true,
+});
+// mongodb://localhost:포트번호/Database이름
+
+const db = mongoose.connection;
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = (error) =>
+	console.log(`❌  Error on DB Connection : ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);

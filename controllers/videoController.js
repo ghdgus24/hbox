80ffcf33d23d7d51c7f076bbd02484808b64ae46
hongsyong92 <ghdgus24@naver.com>
2021-01-11 12:29:20 +0900

    eslint 설정 및 search 완료, webpack 패키지만 설치함

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,7 @@
 import routes from "../routes";
 import Video from "../models/Video";
 
+// Home
 export const home = async (req, res) => {
 	try {
 		const videos = await Video.find({}).sort({ _id: -1 });
@@ -10,12 +11,24 @@ export const home = async (req, res) => {
 		res.render("home", { pageTitle: "Home", videos: [] });
 	}
 };
-export const search = (req, res) => {
+
+// Search
+export const search = async (req, res) => {
 	const {
 		query: { term: searchingBy },
 	} = req;
+	let videos = [];
+	try {
+		videos = await Video.find({
+			title: { $regex: searchingBy, $options: "i" },
+		});
+	} catch (error) {
+		console.log(error);
+	}
 	res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
+
+// Upload
 export const getUpload = (req, res) => {
 	res.render("upload", { pageTitle: "Upload" });
 };
@@ -32,6 +45,7 @@ export const postUpload = async (req, res) => {
 	res.redirect(routes.videoDetail(newVideo.id));
 };
 
+// Video Detail
 export const videoDetail = async (req, res) => {
 	const {
 		params: { id },
@@ -45,6 +59,7 @@ export const videoDetail = async (req, res) => {
 	}
 };
 
+// Edit Video
 export const getEditVideo = async (req, res) => {
 	const {
 		params: { id },
@@ -69,12 +84,15 @@ export const postEditVideo = async (req, res) => {
 	}
 };
 
+// Delete Video
 export const deleteVideo = async (req, res) => {
 	const {
 		params: { id },
 	} = req;
 	try {
 		await Video.findOneAndRemove({ _id: id });
-	} catch (error) {}
+	} catch (error) {
+		console.log(error);
+	}
 	res.redirect(routes.home);
 };
